cb13312b8454e290de2ad3588af2defb71d0108c
Joshua Quinn <comwhizy2k@hotmail.com>
2019-07-04 22:01:56 -0400

    Add project files.

---
+++ CSProject/Manager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CSProject
+{
+    class Manager : Staff
+    {
+        private const float managerHourlyRate = 50;
+
+        public int Allowance
+        {
+            get;  private set;
+
+        }
+
+        public Manager(string name) : base(name, managerHourlyRate)
+        {
+
+        }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+            Allowance = 1000;
+
+            if (HoursWorked > 160)
+            {
+                TotalPay = BasicPay + Allowance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "\nName:" + NameOfStaff + "\nManager Total Pay:" + TotalPay;
+        }
+    }
+}
