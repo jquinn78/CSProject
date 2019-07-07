d43fc36b675570dfec2778785c34e8446075acfe
Joshua Quinn <comwhizy2k@hotmail.com>
2019-07-06 21:36:58 -0400

    Worked on project

---
+++ CSProject/Admin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSProject
+{
+    class Admin : Staff
+    {
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30f;
+
+        public float Overtime { get; private set; }
+
+        public Admin(string name):base (name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if ( HoursWorked > 160)
+            {
+                TotalPay = overtimeRate * (HoursWorked - 160);
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return "\nName:" + NameOfStaff + "\nAdmin Hourly Rate:" + adminHourlyRate + "\nHours Worked:" + HoursWorked + "\nAdmin Basic Pay:" + BasicPay + "\nOvertime:" + Overtime + "\nAdmin Total Pay:" + TotalPay;
+        }
+    }
+}
---
+++ CSProject/FileReader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CSProject
+{
+    class FileReader
+    {
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.txt";
+            string[] seperator = { ", " };
+
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        result = sr.ReadLine().Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+
+                        if (result[1] == "Manager")
+                        {
+                            myStaff.Add(new Manager(result[0]));
+                        }
+                        else
+                        if(result[1] == "Admin")
+                        {
+                            myStaff.Add(new Admin(result[0]));
+
+                        }
+                    }
+
+                    sr.Close();
+                }
+                
+                  
+
+            }
+            else
+            {
+                Console.WriteLine("File Not Found.");
+            }
+
+
+            return myStaff;
+        }
+    }
+}
